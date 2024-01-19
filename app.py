79d0d98fa864dcee7fabf8ad8bcaaca1c4bbcff7
luan <luan640@gmail.com>
2024-01-19 11:07:07 -0300

    novo visual forms

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask_restful import Resource, Api
 from cachetools import Cache
 import cachetools
 
-
 DB_HOST = "database-2.cdcogkfzajf0.us-east-1.rds.amazonaws.com"
 DB_NAME = "postgres"
 DB_USER = "postgres"
@@ -14,9 +13,6 @@ DB_PASS = "15512332"
 app = Flask(__name__)
 api = Api(app)
 
-cache_matriculas = cachetools.LRUCache(maxsize=128)
-
-@cachetools.cached(cache_matriculas)
 def buscar_matriculas():
 
     conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
@@ -48,6 +44,80 @@ class SugestoesFuncionarios(Resource):
 
 api.add_resource(SugestoesFuncionarios, '/sugestoes_funcionarios')
 
+def dados_tabela_requisicao():
+
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
+                            password=DB_PASS, host=DB_HOST)
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    # tabela de requisição 
+    cur.execute(f"SELECT *, data_requisicao - interval '3 hours' AS nova_data FROM requisicao.requisicoes where motivo_exclusao isnull;")
+    requisicoes = cur.fetchall()
+    
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    return requisicoes
+
+def dados_tabela_transferencia():
+
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
+                            password=DB_PASS, host=DB_HOST)
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    # tabela de requisição 
+    cur.execute(f"SELECT * FROM transferencia.transferencias;")
+    transferencias = cur.fetchall()
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    return transferencias
+
+def query_exclusao_requisicao(chave, motivo):
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
+                            password=DB_PASS, host=DB_HOST)
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    sql_update = "update requisicao.requisicoes set motivo_exclusao = %s where id = %s"
+
+    cur.execute(sql_update, (motivo,chave))
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+def query_autorizar_requisicao(chave):
+
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
+                        password=DB_PASS, host=DB_HOST)
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    sql_update = "update requisicao.requisicoes set status_almoxarifado = 'true' where id = %s"
+
+    cur.execute(sql_update, (chave,))
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+@app.route('/inicio', methods=['GET'])
+def inicio():
+
+    # tabela de requisição 
+    requisicoes = dados_tabela_requisicao()
+
+    # tabela de transferência 
+    transferencias = dados_tabela_transferencia()
+
+    return render_template('inicio.html', requisicoes=requisicoes, transferencias=transferencias)
+
 @app.route('/', methods=['GET'])
 def index():
 
@@ -56,7 +126,7 @@ def index():
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
     # Requisição
-    
+
     cur.execute("select concat(matricula, ' - ', nome) as nome_matricula from requisicao.funcionarios")
     matriculas = cur.fetchall()
 
@@ -107,23 +177,42 @@ def classe_por_item():
 
     return jsonify(itens)
 
+# Rota para obter unidade com base no item escolhido
+@app.route('/unidade_por_item', methods=['GET'])
+def unidade_por_item():
+    item = request.args.get('item')
+
+    item = item.split()[0]
+
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    cur.execute("select distinct unidade from requisicao.unidades where codigo = %s", (item,))
+
+    try:
+        unidade = cur.fetchall()[0]
+    except:
+        unidade = 'Não encontrada'
+
+    return jsonify(unidade)
+
 @app.route('/salvar-requisicao', methods=['POST'])
 def salvar_requisicao():
     
-    selectFuncionario = request.form.get('selectFuncionario')
-    selectItem = request.form.get('selectItem')
-    selectCcusto = request.form.get('selectCcusto')
-    selectClasse = request.form.get('selectClasse')
-    quantidade = request.form.get('quantidade')
-    observacao = request.form.get('observacao')
+    funcionario = request.form.get('inputFuncionario')
+    item = request.form.get('inputItem')
+    ccusto = request.form.get('inputCcusto')
+    classe = request.form.get('inputClasse')
+    quantidade = request.form.get('inputQuantidade')
+    observacao = request.form.get('textAreaObservacao')
 
-    print(selectFuncionario,selectItem,selectCcusto,selectClasse,quantidade,observacao)
+    values = [funcionario,ccusto,item,quantidade,classe,observacao]
 
     conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
                             password=DB_PASS, host=DB_HOST)
     cur = conn.cursor()
     
-    cur.execute("INSERT INTO requisicao.requisicoes (funcionario,ccusto,item,quantidade,classe,observacao) VALUES (%s,%s,%s,%s,%s,%s)", (selectFuncionario,selectCcusto,selectItem,quantidade,selectClasse,observacao))
+    cur.execute("INSERT INTO requisicao.requisicoes (funcionario, ccusto, item, quantidade, classe, observacao) VALUES (%s, %s, %s, %s, %s, %s)", tuple(values))
 
     conn.commit()
 
@@ -135,18 +224,16 @@ def salvar_requisicao():
 @app.route('/salvar-transferencia', methods=['POST'])
 def salvar_transferencia():
     
-    selectFuncionario = request.form.get('selectFuncionarioTransferencia')
-    selectItem = request.form.get('selectItemTransferencia')
-    selectDepositoDestino = request.form.get('selectDepositoDestino')
-    quantidade = request.form.get('quantidade')
-
-    print(selectFuncionario,selectItem,quantidade,selectDepositoDestino)
+    funcionario = request.form.get('inputFuncionario')
+    item = request.form.get('inputItem')
+    deposito = request.form.get('inputDeposito')
+    quantidade = request.form.get('inputQuantidade')
 
     conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
                             password=DB_PASS, host=DB_HOST)
     cur = conn.cursor()
     
-    cur.execute("INSERT INTO transferencia.transferencias (funcionario,deposito_origem,recurso,quantidade) VALUES (%s,%s,%s,%s)", (selectFuncionario,selectDepositoDestino,selectItem,quantidade))
+    cur.execute("INSERT INTO transferencia.transferencias (funcionario,deposito_origem,recurso,quantidade) VALUES (%s,%s,%s,%s)", (funcionario,deposito,item,quantidade))
 
     conn.commit()
 
@@ -192,6 +279,72 @@ def registros_transferencias():
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+@app.route('/api/teste/publica/requisicoes', methods=['GET'])
+def registros_requisicoes_teste():
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
+                            password=DB_PASS, host=DB_HOST)
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    cur.execute(f"SELECT * FROM requisicao.requisicoes;")
+    requisicoes = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return jsonify({'data': requisicoes,})
+
+@app.route('/salvar-edicao-tabela-requisicao', methods=['POST'])
+def salvar_edicao_tabela_requisicao():
+
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
+                            password=DB_PASS, host=DB_HOST)
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    chave = request.form.get('chave')
+    item = request.form.get('item')
+    quantidade = request.form.get('quantidade')
+
+    print(chave,item,quantidade)
+
+    sql_query_edit = "update requisicao.requisicoes set item = %s, quantidade = %s where id = %s"
+    cur.execute(sql_query_edit, (item,quantidade,chave,))
+
+    conn.commit()
+
+    sql_consulta = "select * from requisicao.requisicoes"
+    cur.execute(sql_consulta)
+    data = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    print(data)
+
+    return jsonify(data)
+
+@app.route('/autorizar-requisicao', methods=['POST'])
+def autorizar_requisicao():
+
+    chave = request.form.get('chave')
+
+    query_autorizar_requisicao(chave)
+    
+    data = dados_tabela_requisicao()
+
+    return jsonify(data)
+
+@app.route('/exclusao-requisicao', methods=['POST'])
+def exclusao_requisicao():
+
+    chave = request.form.get('chave')
+    motivo = request.form.get('motivo')
+
+    query_exclusao_requisicao(chave,motivo)
+
+    data = dados_tabela_requisicao()
+
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
